3c1b5b2c6136b23d907c3d5ea7f5bf471c7bef4b
Mathewzachariah <mathewzchrh603@gmail.com>
2024-05-23 20:14:33 +0530

    user authentication with token ,(login,create,user validate method

--- chatApp/ApiEndpoints/urls.py
+++ chatApp/ApiEndpoints/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
 
-    path('authenticate_user', views.authenticate_user, name="index"),
-    path('register_new_user', views.register_new_user, name="index"),
+    path('authenticate_user', views.authenticate_user, name="authenticate_user"),
+    path('register_new_user', views.register_new_user, name="register_user"),
     path('tes', views.tesr, name="index"),
 ]
--- chatApp/ApiEndpoints/views.py
+++ chatApp/ApiEndpoints/views.py
@@ -1,4 +1,3 @@
-
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -9,28 +8,31 @@ from .serializer import *
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
+#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
 @authentication_classes([BasicAuthentication])
 @api_view(['POST'])
 def authenticate_user(request):
-    user = User.objects.get(username=""+request.user.username)
+    user = User.objects.get(username="" + request.user.username)
     if user:
         token = Token.objects.get_or_create(user=user)
         print(token[0])
-        return Response({"tokem":str(token[0])})
+        return Response({"token": str(token[0]), "success": True})
     print(request.user.username)
-    return Response("Json")
+    return Response({"message": "Invalid User", "success": False})
+
 
 @api_view(["POST"])
 def register_new_user(request):
-    serializers =UserSerializer(data=request.data)
+    serializers = UserSerializer(data=request.data)
     if serializers.is_valid():
         serializers.save()
-        user =User.objects.get(username=request.data["username"])
+        user = User.objects.get(username=request.data["username"])
         user.set_password(request.data["password"])
         user.save()
-        token =Token.objects.create(user=user)
-        return Response({"tokem":token.key,"user":serializers.data})
-    return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+        token = Token.objects.create(user=user)
+        return Response({"tokem": token.key, "user": serializers.data})
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 @authentication_classes([TokenAuthentication])
